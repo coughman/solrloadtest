6b3744372e670a665f0e03f8b21ca3a3f9d39bb4
Kaufman Ng <kaufman@cloudera.com>
2014-03-25 01:27:13 -0400

    new version using CloudSolrServer

--- src/main/java/com/cloudera/sa/solr/SolrUpdater.java
+++ src/main/java/com/cloudera/sa/solr/SolrUpdater.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -12,6 +13,7 @@ import java.util.UUID;
 
 import org.apache.log4j.Logger;
 import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.CloudSolrServer;
 import org.apache.solr.client.solrj.impl.ConcurrentUpdateSolrServer;
 import org.apache.solr.common.SolrInputDocument;
 
@@ -22,79 +24,149 @@ public class SolrUpdater {
 	static Logger logger = Logger.getLogger(SolrUpdater.class);
 	
 	static ArrayList<JsonNode> records = new ArrayList<JsonNode>(); 
-	static ConcurrentUpdateSolrServer solrServer = null;
 
 	private static final String NATIVE_SOLR_DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"; // e.g. 2007-04-26T08:05:04.789Z
 	private static final SimpleDateFormat SOLR_DATE_FORMAT = new SimpleDateFormat(NATIVE_SOLR_DATE_FORMAT);
 	
 	public static void main(String[] args) throws IOException, SolrServerException {
-		if (args.length != 5) {
-			System.err.println("synopsis:\njava -cp SolrLoadTest.jar com.cloudera.sa.solr.SolrUpdater <solr_url> <json_data_file> <num_threads> <solr_queue_size> <total_num_records>");
+		if (args.length != 7) {
+			System.err.println("synopsis:\njava -cp SolrLoadTest.jar com.cloudera.sa.solr.SolrUpdater <zk_host> <solr_url> <json_data_file> <num_threads> <solr_queue_size> <total_num_records>");
 			return;
 		}
-		int numThreads = Integer.parseInt(args[2]);
-		int queueSize = Integer.parseInt(args[3]);
-		int totalRecords = Integer.parseInt(args[4]);
 		
-		String inputFile = args[1];
+		String zkHost = args[0];
+		//String solrUrl = args[1];
+		String collectionName = args[2];
+		String inputFile = args[3];
+		int numThreads = Integer.parseInt(args[4]);
+		//int queueSize = Integer.parseInt(args[5]);
+		int totalRecords = Integer.parseInt(args[6]);
 		
-		loadInputFileIntoMemory(inputFile);
-		solrServer = new ConcurrentUpdateSolrServer(args[0], queueSize, numThreads);
+		loadInputFileIntoMemory(inputFile);	
 		
-		long startTime = Calendar.getInstance().getTimeInMillis();
-		
-		doConcurrentUpdate(totalRecords);
-		
-		long elapsedTime = Calendar.getInstance().getTimeInMillis() - startTime;
-		logger.info("finished adding " + totalRecords + " documents");
-		logger.info("Update elapsed time: " + elapsedTime/1000.0 + "s");
-		
-		logger.info("begin committing");
-		startTime = Calendar.getInstance().getTimeInMillis();
-		solrServer.commit();
-		logger.info("finished committing.  Commit elapsed time: " + (Calendar.getInstance().getTimeInMillis() - startTime)/1000.0 + "s");
+		doCloudSolrUpdate(totalRecords, numThreads, zkHost, collectionName);
+		//doConcurrentUpdate(totalRecords, solrUrl, queueSize, numThreads);
+				
 		logger.info("waiting for threads to die...");
 	}
 
-/*	private static void doCloudSolrUpdate(int numRecords, int numThreads) {
+	static void doCloudSolrUpdate(int numRecords, int numThreads, String zkHost, String collectionName) throws MalformedURLException {
+		logger.info("using CloudSolrServer class for indexing...");
+		int numRecordsLeft = numRecords;
+		
+		if (numRecords < numThreads) {
+			logger.error("number of records must be larger than number of threads.  Aborting...");
+			return;
+		}
+		
+		int numRecordsPerThread = numRecords / numThreads;
 		for (int i = 0; i < numThreads; i++) {
-			Thread t = new Thread(new SolrUpdateRunnable());
+			Thread t = new Thread(new SolrUpdateRunnable(zkHost, 
+					(numRecordsLeft > numRecordsPerThread) ? numRecordsPerThread : numRecordsLeft, collectionName));
 			t.start();
-					
+			numRecordsLeft-=numRecordsPerThread;
 		}
 	}
 	
 	static class SolrUpdateRunnable implements Runnable {
+		String zkHost;
 		static Logger logger = Logger.getLogger(SolrUpdateRunnable.class);
+		CloudSolrServer solrServer;
+		int numRecords;
+		
+		public SolrUpdateRunnable(String zkHost, int numRecords, String collectionName) throws MalformedURLException
+		{
+			this.zkHost = zkHost;
+			this.solrServer = new CloudSolrServer(zkHost);
+			this.solrServer.setDefaultCollection(collectionName);
+			this.numRecords = numRecords;
+		}
 		
 		public void run() {
-			// TODO Auto-generated method stub
+			ArrayList<SolrInputDocument> docs = new ArrayList<SolrInputDocument>(numRecords);
+			int currentIndex = 0;
+
+			for (int i = 0; i < numRecords; i++) {
+				SolrInputDocument doc = parseDocument(records.get(currentIndex++));
+				docs.add(doc);
+				if (currentIndex == records.size())
+					currentIndex = 0;				
+			}
+			long startTime;
+			
+			try {
+				startTime = Calendar.getInstance().getTimeInMillis();
+				solrServer.add(docs);
+				long elapsedTime = Calendar.getInstance().getTimeInMillis() - startTime;
+				logger.info("finished adding " + numRecords + " documents");
+				logger.info("Update elapsed time: " + elapsedTime/1000.0 + "s");
+			} catch (Exception e) {
+				logger.error("failed to add solr docs", e);
+			} 
+			
+			try {
+				logger.info("begin committing");
+				startTime = Calendar.getInstance().getTimeInMillis();				
+				solrServer.commit();
+				logger.info("finished committing.  Commit elapsed time: " + (Calendar.getInstance().getTimeInMillis() - startTime)/1000.0 + "s");		
+			} catch (Exception e) {
+				logger.error("failed to commit", e);
+			}
+			finally {
+				solrServer.shutdown();
+			}
 			
 		}
 		
-	}*/
+	}
 	
 	/**
 	 * Update solr docs using Solr ConcurrentUpdateSolrServer class
 	 * @param totalRecords
+	 * @param numThreads 
+	 * @param queueSize 
+	 * @param solrUrl 
 	 * @throws IOException
 	 */
-	private static void doConcurrentUpdate(int totalRecords)
-			throws IOException {
+	static void doConcurrentUpdate(int totalRecords, String solrUrl, int queueSize, int numThreads) {
+		logger.info("using ConcurrentUpdateSolrServer class for indexing...");
+
+		ConcurrentUpdateSolrServer solrServer = new ConcurrentUpdateSolrServer(solrUrl, queueSize, numThreads);
 		
 		int currentIndex = 0;
+		ArrayList<SolrInputDocument> docs = new ArrayList<SolrInputDocument>(totalRecords);
 
+		long startTime = Calendar.getInstance().getTimeInMillis();
+		
 		for (int i = 0; i < totalRecords; i++) {
 			SolrInputDocument doc = parseDocument(records.get(currentIndex++));
-			try {
-				logger.trace("adding solr doc:" + doc);
-				solrServer.add(doc);
-			} catch (SolrServerException e) {
-				logger.error("failed to add document:" + doc, e);
-			}
-			
+
+			logger.trace("adding solr doc:" + doc);
+			docs.add(doc);
+				
 			if (currentIndex == records.size())
 				currentIndex = 0;
+		}
+		
+		try {
+			solrServer.add(docs);
+			long elapsedTime = Calendar.getInstance().getTimeInMillis() - startTime;
+			logger.info("finished adding " + totalRecords + " documents");
+			logger.info("Update elapsed time: " + elapsedTime/1000.0 + "s");			
+		} catch (Exception e) {
+			logger.error("failed to add docs", e);
+		}
+		
+		try {	
+			logger.info("begin committing");
+			startTime = Calendar.getInstance().getTimeInMillis();
+			solrServer.commit();
+			logger.info("finished committing.  Commit elapsed time: " + (Calendar.getInstance().getTimeInMillis() - startTime)/1000.0 + "s");		
+		} catch (Exception e) {
+			logger.error("failed to commit", e);
+		}
+		finally {
+			solrServer.shutdown();
 		}
 	}
 
